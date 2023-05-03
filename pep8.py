292c8b93b9614609c491a2832eb793bab54d35be
MiriamPerez24 <perezm58@uabc.edu.mx>
2023-05-02 19:40:28 -0700

    Archivo sin archivo pep8

---
+++ pep8.py
@@ -0,0 +1,10 @@
+class User():
+    def  __init__(self, username,password=''):
+
+    self.usarname = username
+    self.password = password
+
+    def set_password(self):
+        pass
+
+    condyuser = user('Cody')
